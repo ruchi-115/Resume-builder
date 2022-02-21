6a00d66c64e242d609154cccf8c66a19282eba2f
ruchi-115 <ruchitasonawale115@gmail.com>
2022-02-21 11:44:24 +0530

    added homepage

--- src/App.js
+++ src/App.js
@@ -2,45 +2,56 @@ import Template1 from "./Components/Template1/Template1";
 import Template2 from "./Components/Template2/Template2";
 import Template3 from "./Components/Template3/Template3";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import "./App.css";
 
 function App() {
   return (
     <Router>
       <nav
-        className="nav d-flex ps-4 align-items-center"
+        className="nav d-flex ps-5 align-items-center"
         style={{ height: "8vh", background: "#6D4Fc4" }}
       >
-        <Link className="navbar-brand fs-1 text-white" to="/">
+        <Link className="navbar-brand fs-3 text-white" to="/">
           <i
-            class="bi bi-file-earmark-break-fill"
+            class="bi bi-file-earmark-break-fill pb-2"
             style={{ fontSize: "40px" }}
           ></i>
-          ResumeBuilder
+          ResumeB
         </Link>
-        <Link className="nav-link fs-4 text-white" to="/template">
-          Tempaltes
+        <Link className="nav-link fs-5 text-white pb-0 rounded" to="/">
+          Home
+        </Link>
+        <Link className="nav-link fs-5 text-white pb-0 rounded" to="/template">
+          Templates
         </Link>
       </nav>
       <Switch>
         <Route exact path="/">
           <section
-            className="d-flex  justify-content-around flex-column align-items-center container p-3"
-            style={{ width: "100%", height: "92vh" }}
+            className=""
           >
-            <h1 className="">Welcome to our Resume Website</h1>
-            <div className="text-center">
-              Cover is a one-page template for building simple and beautiful
-              home pages. Download, edit the text, and add your own fullscreen
-              background photo to make it your own.
+            <div className="container-fluid d-flex justify-content-between rounded my-3 mx-5 shadow-lg" style={{ width: "auto", height: "auto" }}>
+              <div className="pt-5 ">
+                <img src="assets/img1.png" width="450px" height="200px" className="home-img img-fluid" alt="home img"/>
+              </div>
+              <div className=" my-5" style={{padding: "8rem 4rem"}}>
+                <h1 className="bold fs-1">Online Resume Builder</h1>
+                  <p className="fs-3">Effortlessly make a job-worthy resume <p>and get hired faster</p></p>
+                <Link
+                  to="/template"
+                  className="btn btn-lg fw-bold text-white my-3 shadow-lg"
+                  style={{ background: "#5049C5" }}
+                >
+                  Start Now
+                </Link>
+              </div>
+              <div style={{padding: "0rem 0rem 0rem 0rem"}}>
+                <img src="assets/img2(1).png" width="250px" height="300px" className="home-img img-fluid" alt="home img" />
+              </div>
+              
             </div>
-            <Link
-              to="/template"
-              className="btn btn-lg  fw-bold text-white my-5 shadow"
-              style={{ background: "#6D4Fc4" }}
-            >
-              Create Resume
-            </Link>
           </section>
+
         </Route>
         <Route exact path="/template">
           <div className="mt-5">
@@ -57,21 +68,21 @@ function App() {
                 to="/template/1"
                 style={{ width: "350px" }}
               >
-                <img src="1.jpg" className="card-img" alt="..." />
+                <img src="assets/1.jpg" className="card-img" alt="..." />
               </Link>
               <Link
                 className="shadow mx-2"
                 to="/template/2"
                 style={{ width: "350px" }}
               >
-                <img src="2.jpg" className="card-img" alt="..." />
+                <img src="assets/2.jpg" className="card-img" alt="..." />
               </Link>
               <Link
                 className="shadow mx-2"
                 to="/template/3"
                 style={{ width: "350px" }}
               >
-                <img src="3.jpg" className="card-img" alt="..." />
+                <img src="assets/3.jpg" className="card-img" alt="..." />
               </Link>
             </div>
           </div>
